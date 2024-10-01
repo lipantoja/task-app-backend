34fd1f4aecd714346ef7a5ac9e66fb48c72aa35e
Laura Pantoja <113263904+lipantoja@users.noreply.github.com>
2024-10-01 17:04:50 -0400

    added show action

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -3,4 +3,30 @@ class TodosController < ApplicationController
     @todos = Todo.all
     render :index
   end
+  def create
+    @todo =Todo.new(
+      user_id: params[:user_id],
+      category_id: params[:category_id],
+      title: params[:title],
+      description: params[:description],
+      deadline: params[:deadline],
+      completed: params[:completed],
+    )
+    if @todo.save
+      render :show
+    else
+      render json: {errors: @todo.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+  def show
+    @todo = Todo.find_by(id: params[:id])
+    render :show
+  end
+
+  def update
+    #placeholder
+  end
+  def destroy
+    #placeholder
+  end
 end
--- test/controllers/todos_controller_test.rb
+++ test/controllers/todos_controller_test.rb
@@ -1,7 +1,20 @@
 require "test_helper"
 
 class TodosControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
+  test "index" do
+    get "/todos.json"
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal Todo.count, data.length
+  end
+  # tested index
+  
+  # test "create" do
+  #   assert_difference "Todo.count", 1 do
+  #     post "/todos.json", params: { name: "lake", width: 800, height: 600 }
+  #     assert_response 200
+
+  #   end
   # end
 end

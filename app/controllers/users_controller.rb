e2d2815f2981614485046688cf0f0d24c2ce72c8
amberdemirlika <amberdemir12@gmail.com>
2024-10-01 19:51:32 -0500

    add user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,32 +13,26 @@ class UsersController < ApplicationController
     end
   end
 
-  def index
-    @users = User.all
-    render json: @users
-  end
+  #   def index
+  #     @users = User.all
+  #     render json: @users
+  #   end
 
-  def show
-    @user = User.find_by(id: params[:id])
-    render :show
-  end
+  #   def show
+  #     @user = User.find_by(id: params[:id])
+  #     render :show
+  #   end
 
-  def update
-    @user = User.find_by(id: params[:id])
+  #   def update
+  #     @user = User.find_by(id: params[:id])
 
-    if @user.update(
-      name: params[:name] || @user.name,
-      email: params[:width] || @user.email,
-    )
-      render json: @user, status: :ok # Return the updated user as JSON
-    else
-      render json: { errors: @user.errors.full_messages }, status: :bad_request
-    end
-  end
-
-  def destroy
-    @user = User.find_by(id: params[:id])
-    @user.destroy
-    render json: { message: "User destroyed successfully" }
-  end
+  #     if @user.update(
+  #       name: params[:name] || @user.name,
+  #       email: params[:width] || @user.email,
+  #     )
+  #       render json: @user, status: :ok # Return the updated user as JSON
+  #     else
+  #       render json: { errors: @user.errors.full_messages }, status: :bad_request
+  #     end
+  #   end
 end
